40b419f59785e541eca914df1f676eb712abeace
Martin-Louis Bright <mlbright@gmail.com>
2021-09-18 23:26:50 -0400

    Cleanup

--- run.py
+++ run.py
@@ -1,8 +1,5 @@
 #!/usr/local/bin/python3
 
-import time
-import pprint
-
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
@@ -10,34 +7,57 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support.expected_conditions import presence_of_element_located
 from selenium.webdriver.support.ui import Select
 
-with open('/tmp/pass') as file:
+user = "mlbright@gmail.com"
+
+with open("/tmp/goodreads-pass") as file:
     password = file.read()
 
-pp = pprint.PrettyPrinter(indent=4)
+driver = webdriver.Firefox()
+wait = WebDriverWait(driver, 10)
 
-with webdriver.Firefox() as driver:
-    wait = WebDriverWait(driver, 10)
-    driver.get("https://www.goodreads.com/user/sign_in")
-    driver.find_element(By.NAME, "user[email]").send_keys("mlbright@gmail.com")
-    driver.find_element(By.NAME, "user[password]").send_keys(password + Keys.RETURN)
+# Sign in to goodreads.com
+driver.get("https://www.goodreads.com/user/sign_in")
+driver.find_element(By.NAME, "user[email]").send_keys(user)
+driver.find_element(By.NAME, "user[password]").send_keys(password + Keys.RETURN)
 
-    wait.until(presence_of_element_located((By.CSS_SELECTOR,".siteHeader__primaryNavInline > ul:nth-child(1) > li:nth-child(2) > a:nth-child(1)")))
+# Wait for the 'My Books' link to show up, and click it
+wait.until(
+    presence_of_element_located(
+        (
+            By.CSS_SELECTOR,
+            ".siteHeader__primaryNavInline > ul:nth-child(1) > li:nth-child(2) > a:nth-child(1)",
+        )
+    )
+)
+driver.find_element(
+    By.CSS_SELECTOR,
+    ".siteHeader__primaryNavInline > ul:nth-child(1) > li:nth-child(2) > a:nth-child(1)",
+).click()
 
-    driver.find_element(By.CSS_SELECTOR, ".siteHeader__primaryNavInline > ul:nth-child(1) > li:nth-child(2) > a:nth-child(1)").click()
-    wait.until(presence_of_element_located((By.CSS_SELECTOR,".selectedShelf")))
+# Wait for the 'Read' link to appear
+wait.until(presence_of_element_located((By.CSS_SELECTOR, ".selectedShelf")))
 
-    driver.get("https://www.goodreads.com/review/list/3123130-martin-louis-bright?shelf=read")
-    wait.until(presence_of_element_located((By.CSS_SELECTOR,"#per_page")))
-    select = Select(driver.find_element_by_id('per_page'))
+# Click the 'Read' link
+driver.get(
+    "https://www.goodreads.com/review/list/3123130-martin-louis-bright?shelf=read"
+)
 
-    # select.select_by_visible_text("infinite scroll")
-    select.select_by_visible_text("100")
+# Wait until the pagination dropdown shows up
+wait.until(presence_of_element_located((By.CSS_SELECTOR, "#per_page")))
 
-    table = driver.find_element_by_id("books")
-    rows = table.find_elements_by_tag_name("tr")
+# Select 100 books per page
+select = Select(driver.find_element_by_id("per_page"))
+select.select_by_visible_text("100")
 
-    for i in range(len(rows)):
-        cells = rows[i].find_elements_by_tag_name("td")
-        if len(cells) >= 8:
-            fields = [c.text for c in cells[3:7]]
-            print("{}".format("\t".join(fields)))
+# Find all the rows in the books table
+table = driver.find_element_by_id("booksBody")
+rows = table.find_elements_by_tag_name("tr")
+
+# For each row, take the book title, author, and date added
+for i in range(len(rows)):
+    cells = rows[i].find_elements_by_tag_name("td")
+    fields = [c.text for c in cells[3:5]]
+    fields.append(cells[23].text)
+    print("\t".join(fields))
+
+driver.quit()
